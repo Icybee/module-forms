bf52a5bd8a94764ca2b4284b3598528ce36fe210
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 02:30:40 +0200

    Event config fragments are now their own file

---
+++ config/event.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+	'ICanBoogie\Operation::get_form' => 'Icybee\Modules\Forms\Hooks::on_operation_get_form'
+
+];
--- config/hooks.php
+++ config/hooks.php
@@ -2,12 +2,6 @@
 
 return [
 
-	'events' => [
-
-		'ICanBoogie\Operation::get_form' => 'Icybee\Modules\Forms\Hooks::on_operation_get_form'
-
-	],
-
 	'patron.markups' => [
 
 		'feedback:form' => [
